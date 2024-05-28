820d119381cab59b336e141fe1480f4ac0c5c8f6
c9yu <polkj11290@naver.com>
2024-05-28 14:23:14 +0900

    20240528

--- part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
+++ part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using LiveChartsCore.SkiaSharpView.Extensions;
+﻿using LiveChartsCore.SkiaSharpView.Drawing;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.VisualElements;
 using MahApps.Metro.Controls;
 using MQTTnet;
 using MQTTnet.Client;
@@ -22,6 +25,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 
 namespace SmartHomeMonitoringApp.Views
 {
@@ -42,12 +48,15 @@ namespace SmartHomeMonitoringApp.Views
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            LblSensingDt.Content = DateTime.Now.ToString();
+            LblSensingDt.Content = DateTime.Now.ToString("yyyy-MM-dd-HH");
         }
 
         private async void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            if(Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+
+
+
+            if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
             { // 이미 다른 화면에서 MQTT 를 연결했다면
                 Commons.MQTT_CLIENT.ApplicationMessageReceivedAsync += MQTT_CLIENT_ApplicationMessageReceivedAsync;
             }
@@ -56,7 +65,7 @@ namespace SmartHomeMonitoringApp.Views
                 var mqttFactory = new MqttFactory();
                 Commons.MQTT_CLIENT = mqttFactory.CreateMqttClient();
                 var mqttClientQptions = new MqttClientOptionsBuilder().WithTcpServer(Commons.BROKERHOST).Build();
-                await Commons.MQTT_CLIENT.ConnectAsync(mqttClientQptions,CancellationToken.None);
+                await Commons.MQTT_CLIENT.ConnectAsync(mqttClientQptions, CancellationToken.None);
                 Commons.MQTT_CLIENT.ApplicationMessageReceivedAsync += MQTT_CLIENT_ApplicationMessageReceivedAsync;
 
                 var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter(
@@ -77,6 +86,20 @@ namespace SmartHomeMonitoringApp.Views
             return Task.CompletedTask;
         }
 
+        // 앵귤러차트를 위한 변수, 속성 선언
+        public IEnumerable<ISeries> HumidSeries { get; set; }
+
+        public IEnumerable<VisualElement<SkiaSharpDrawingContext>> VisualElements { get; set; }
+
+        public NeedleVisual Needle { get; set; }
+
+        private static void SetStyle(
+        double sectionsOuter, double sectionsWidth, PieSeries<ObservableValue> series)
+        {
+            series.OuterRadiusOffset = sectionsOuter;
+            series.MaxRadialColumnWidth = sectionsWidth;
+        }
+
         private void UpdateChart(string payload)
         {
             // 차트에 값 대입해서 차트가 나오도록 함
@@ -88,6 +111,7 @@ namespace SmartHomeMonitoringApp.Views
                 var temp = Convert.ToDouble(splitValue[0]);
                 var humid = Convert.ToDouble(splitValue[1]);
 
+                // 온도차트 값
                 var tempVal = GaugeGenerator.BuildSolidGauge(new GaugeItem(
                         temp,
                         series =>
@@ -96,8 +120,52 @@ namespace SmartHomeMonitoringApp.Views
                             series.DataLabelsSize = 50;
                         }
                     ));
-                ChtDinningTemp.Series = tempVal;
+                ChtLivingTemp.Series = ChtDinningTemp.Series = ChtBedTemp.Series = ChtBathTemp.Series = tempVal;
+
+                // 습도를 나타낼 앵귤러 차트를 초기화
+                var sectionOuter = 130;
+                var sectionWidth = 20;
+
+            // 습도차트 값 할당
+                HumidSeries = GaugeGenerator.BuildAngularGaugeSections(
+                    new GaugeItem(humid,
+                    s => SetStyle(sectionOuter, sectionWidth, s))
+                );
+                ChtLivingHumid.Series = ChtDiningHumid.Series = ChtBedHumid.Series = ChtBathHumid.Series = HumidSeries;
+
+                // 습도를 나타낼 앵귤러 차트를 초기화
+                Needle = new NeedleVisual { Value = humid };
+
+                VisualElements = new VisualElement<SkiaSharpDrawingContext>[]
+                {
+                new AngularTicksVisual // 앵귤러 차트를 그리기 위한 속성들
+                {
+                    LabelsSize = 16,
+                    LabelsOuterOffset = 15,
+                    OuterOffset = 65,
+                    TicksLength = 20
+                },
+                Needle
+                };
+
+                ChtLivingHumid.VisualElements = ChtDiningHumid.VisualElements = ChtBedHumid.VisualElements = ChtBathHumid.VisualElements = VisualElements; // 위에서 만든 VisualElements(화면 디자인)을 차트에 적용
+
             });
+        }
+
+        private void BtnWarning_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control' : 'warning'}");
+        }
+
+        private void BtnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control' : 'normal'}");
+        }
+
+        private void BtnOff_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control' : 'off'}");
         }
     }
 }
